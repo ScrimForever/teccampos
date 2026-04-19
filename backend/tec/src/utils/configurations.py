ddac84aae9d2ca11ca9bf08b7ab18c4f2ebbd83c
Thiago Salgado <thiago.salgado.monteiro@gmail.com>
2026-04-19 12:05:31 -0300

    feat: database correctly

--- backend/tec/src/utils/configurations.py
+++ backend/tec/src/utils/configurations.py
@@ -1,13 +1,16 @@
 from pydantic_settings import SettingsConfigDict, BaseSettings
-
+from pydantic import computed_field
 
 class Configuration(BaseSettings):
 
     model_config = SettingsConfigDict()
 
     X_S: str = ""
-    DATABASE_URL: str = f"postgresql+asyncpg://postgres:{X_S}@localhost/postgres"
 
+    @computed_field
+    @property
+    def DATABASE_URL(self) -> str:
+        return f"postgresql+asyncpg://postgres:{self.X_S}@localhost/postgres"
 
 config = Configuration()
 
